a71f3395fd62b3657d0416f07ec03b5c40f53576
Yohan LEE <alien2327@gmail.com>
2023-04-03 12:00:36 +0900

    Update myvim.sh

--- myvim.sh
+++ myvim.sh
@@ -7,3 +7,8 @@ curl -sL install-node.vercel.app/lts | bash
 
 curl -fLo ~/.vimrc \
     https://raw.githubusercontent.com/alien2327/alien2327/main/vimrc
+
+echo "Post setting"
+echo "CocInstall coc-json coc-tsserver coc-pyright"
+echo "LSP: https://github.com/neoclide/coc.nvim/wiki/Language-servers"
+echo ""
