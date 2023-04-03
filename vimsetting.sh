465e84c88aca97b8f2a312018a887feb9dfe00be
Yohan LEE <alien2327@gmail.com>
2023-04-03 11:53:00 +0900

    Create vimsetting.sh

---
+++ vimsetting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -sL install-node.vercel.app/lts | bash
+
