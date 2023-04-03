db0d254d12f79d09b84d0f370b9af075c2097cf0
Yohan LEE <alien2327@gmail.com>
2023-04-03 11:53:56 +0900

    Update and rename vimsetting.sh to myvim.sh

--- myvim.sh
+++ myvim.sh
@@ -5,3 +5,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 curl -sL install-node.vercel.app/lts | bash
 
+curl -fLo ~/.vimrc \
+    https://raw.githubusercontent.com/alien2327/alien2327/main/vimrc
