f1d62334251c2668ba377921df9ed0f6e3a41544
Yohan LEE <alien2327@gmail.com>
2023-04-04 09:14:56 +0900

    Update myvim.sh

--- myvim.sh
+++ myvim.sh
@@ -11,4 +11,4 @@ curl -fLo ~/.vimrc \
 echo "Post setting"
 echo "CocInstall coc-json coc-tsserver coc-pyright"
 echo "LSP: https://github.com/neoclide/coc.nvim/wiki/Language-servers"
-echo ""
+echo "coc-json coc-tsserver coc-cmake coc-clangd coc-pyright coc-sh coc-go coc-java coc-html coc-rls"
